f0297f193cd67fa1126ea3443c7b86348455287c
Anand Dukkipati <aRdukkipati@gmail.com>
2025-11-06 13:30:50 -0600

    finished project setup. Beginning to parse arguments and attach to process

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -1,6 +1,27 @@
+#include <sys/ptrace.h>
+#include <unistd.h>
+
+#include <iostream>
 #include <libsdb/libsdb.hpp>
+#include <string_view>
 
-int main() {
-    sdb::say_hello();
+namespace {
+pid_t attach(int argc, const char **argv) {
+  pid_t pid = 0;
+  // Passing PID
+  if (argc == 3 && argv[1] == std::string_view("-p")) {
+    // Passing program name
+  } else {
+  }
+  return pid;
 }
+} // namespace
 
+int main(int argc, const char **argv) {
+  if (argc == 1) {
+    std::cerr << "No arguments given\n";
+    return -1;
+  }
+
+  pid_t pid = attach(argc, argv);
+}
